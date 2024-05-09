b99cad9045b1879d1c4b8a2bacd79357d0929c8e
yuta-hamasaki <yh02052002@gmail.com>
2024-05-08 19:01:44 -0700

    Update:installed tailwind.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='bg-red-500 text-center'>
       Header
     </div>
   )
--- src/pages/top/Top.jsx
+++ src/pages/top/Top.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div>
+    <div className='text-blue bg-red-500'>
       {/*トップページをここに記述 */}
       top
     </div>
