78a013ad00a73f1a18d1ff69e4c26267a6f9a9c4
Hiroki Ueno <rockyueno@gmail.com>
2024-05-10 16:08:21 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Top from './pages/top/Top';
 import Page404 from "./pages/404/Page404";
+import ProductPost from "./pages/ProductPost";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/*サインアップページ */}
         {/*商品詳細*/}
         {/*商品投稿 */}
+        <Route path="/productpost" element={<ProductPost/>}/>
         {/*プロフィール*/}
         {/*アカウント設定 */}
         <Route path="*" element={<Page404/>}/> {/*404ページ */}
---
+++ src/pages/ProductPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductPost = () => {
+  return (
+    <div className='text-blue bg-red-500'>
+      {/*トップページをここに記述 */}
+      HelloProductPost
+    </div>
+  )
+}
+
+export default ProductPost
