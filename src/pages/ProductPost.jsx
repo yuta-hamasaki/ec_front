b48c997b4ec2a320e759245a4c671e3de0e07276
Hiroki Ueno <rockyueno@gmail.com>
2024-05-10 17:00:24 -0700

    Add website components

--- src/pages/ProductPost.jsx
+++ src/pages/ProductPost.jsx
@@ -4,7 +4,16 @@ const ProductPost = () => {
   return (
     <div className='text-blue bg-red-500'>
       {/*トップページをここに記述 */}
-      HelloProductPost
+      <div className='product-img'></div>
+      <p className='product-name'>Product Name</p>
+      <input type="text" name="product-name" id="product-name" />
+      <p className='product-category'>Category</p>
+      <select name="product-category" id="product-category"></select>
+      <p className='product-price'>Price</p>
+      <input type="text" name="product-price" id="product-price" />
+      <p className='product-description'>Description</p>
+      <textarea name="product-description" id="product-description"></textarea>
+      <input className='post-product' type="submit" value="Post Product" />
     </div>
   )
 }
