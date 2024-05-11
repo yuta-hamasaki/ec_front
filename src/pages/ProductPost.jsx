2cb93211db10134a8f03d16e2b0e23995c8d663e
Hiroki Ueno <rockyueno@gmail.com>
2024-05-11 16:27:52 -0700

    Update basic style

--- src/pages/ProductPost.jsx
+++ src/pages/ProductPost.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const ProductPost = () => {
   return (
     <div className='flex justify-center'>
-      <div className=''>
+      <div className='w-full sm:w-96'>
         <div className='h-48'></div>
         <p className='mb-2 mt-4'>Product Name</p>
-        <input type="text" name="product-name" id="product-name" className='' />
+        <input type="text" name="product-name" id="product-name" className='w-full box-border border border-slate-400 rounded p-1' />
         <p className='mb-2 mt-4'>Category</p>
-        <select name="product-category" id="product-category"></select>
+        <select name="product-category" id="product-category" className='w-full box-border border border-slate-400 rounded p-1'></select>
         <p className='mb-2 mt-4'>Price</p>
-        <input type="text" name="product-price" id="product-price" />
+        <input type="text" name="product-price" id="product-price" className='w-full box-border border border-slate-400 rounded p-1' />
         <p className='mb-2 mt-4'>Description</p>
-        <textarea name="product-description" id="product-description"></textarea>
+        <textarea name="product-description" id="product-description" className='w-full h-32 box-border border border-slate-400 rounded p-1'></textarea>
         <br />
-        <input className='bg-blue-500 rounded-lg border-neutral-500' type="submit" value="Post Product" />
+        <input className='bg-indigo-500 text-white rounded mt-6 h-8 w-28 block mx-auto text-xs' type="submit" value="Post Product" />
       </div>
     </div>
   )
