c193e52fd785ec79c86a39d63b7f7fb7d4bfa6cd
Hiroki Ueno <rockyueno@gmail.com>
2024-05-10 19:01:22 -0700

    Update basic style

--- src/pages/ProductPost.jsx
+++ src/pages/ProductPost.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 const ProductPost = () => {
   return (
-    <div className='text-blue bg-red-500'>
-      {/*トップページをここに記述 */}
-      <div className='product-img'></div>
-      <p className='product-name'>Product Name</p>
-      <input type="text" name="product-name" id="product-name" />
-      <p className='product-category'>Category</p>
-      <select name="product-category" id="product-category"></select>
-      <p className='product-price'>Price</p>
-      <input type="text" name="product-price" id="product-price" />
-      <p className='product-description'>Description</p>
-      <textarea name="product-description" id="product-description"></textarea>
-      <input className='post-product' type="submit" value="Post Product" />
+    <div className='flex justify-center'>
+      <div className=''>
+        <div className='h-48'></div>
+        <p className='mb-2 mt-4'>Product Name</p>
+        <input type="text" name="product-name" id="product-name" className='' />
+        <p className='mb-2 mt-4'>Category</p>
+        <select name="product-category" id="product-category"></select>
+        <p className='mb-2 mt-4'>Price</p>
+        <input type="text" name="product-price" id="product-price" />
+        <p className='mb-2 mt-4'>Description</p>
+        <textarea name="product-description" id="product-description"></textarea>
+        <br />
+        <input className='bg-blue-500 rounded-lg border-neutral-500' type="submit" value="Post Product" />
+      </div>
     </div>
   )
 }
