846ad158d0d7399639c6e8a328c086c881b497bc
yuta-hamasaki <yh02052002@gmail.com>
2024-05-13 15:00:55 -0700

    update: product-list and card

--- src/pages/top/Top.jsx
+++ src/pages/top/Top.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import ProductCard from '../../components/productCard/ProductCard'
 
 const Top = () => {
+
+const productData = [
+  {
+  id:1, productName:'Shampoo', shopName: "Yuta .inc", price:"$100.00",
+},
+{id:2, productName:'Yuta ', shopName: "cornerstone .inc", price:"$100.00"
+},
+{id:2, productName:'Yuta ', shopName: "cornerstone .inc", price:"$100.00"
+},
+{id:2, productName:'Yuta ', shopName: "cornerstone .inc", price:"$100.00"
+},
+{id:2, productName:'Yuta ', shopName: "cornerstone .inc", price:"$100.00"
+},
+{id:2, productName:'Yuta ', shopName: "cornerstone .inc", price:"$100.00"
+},
+];
+
   return (
     <>
     {/*トップページをここに記述 */}
@@ -9,8 +27,15 @@ const Top = () => {
       <div className="">
         {/*トップセクション */}
       </div>
-      <div className="">
-        {/* <productCard/>*/}
+      <div className="flex flex-wrap m-6">
+        {productData.map((product, index)=>(
+          <ProductCard
+            key={index}
+            productName={product.productName}
+            shopName={product.shopName}
+            price={product.price}
+          />
+        ))}
       </div>
     </div>
     </>
