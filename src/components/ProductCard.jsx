050305014b139aba34648557118c78156384808f
yuta-hamasaki <yh02052002@gmail.com>
2024-05-21 13:29:04 -0700

    Updated style of  Card & Top

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProductCard = ({ productName, shopName, price }) => {
   return (
-    <div className="rounded-lg border border-solid w-full md:w-1/3 lg:w-1/4 p-4 m-2">
+    <div className="rounded-lg w-full md:w-1/3 lg:w-1/4 p-4 m-2 shadow-xl">
       <div className="flex flex-col items-center">
         <img
           src="https://images.unsplash.com/photo-1695048401313-d7153c37d6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
