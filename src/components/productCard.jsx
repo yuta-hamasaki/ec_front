fc57165783d2f52b3492010ce235d569e3451c0f
yuta-hamasaki <yh02052002@gmail.com>
2024-05-21 12:52:50 -0700

    Updated filename

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -8,6 +8,7 @@ const ProductCard = ({productName, shopName, price}) => {
           {/*<img src="" alt="" /> Product img*/}
           <img src="https://images.unsplash.com/photo-1695048401313-d7153c37d6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product" />
         </div>
+        {/* <Link></Link> */}
         <div className="text-center">
           <h1>{productName}</h1>
           <p>{shopName}</p>
