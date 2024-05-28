f21bb0c04fb7c030790f98925ec3ad247610a6a2
Hiroki Ueno <rockyueno@gmail.com>
2024-05-27 17:34:31 -0700

    Add form tag

--- src/pages/ProductPost.jsx
+++ src/pages/ProductPost.jsx
@@ -5,20 +5,22 @@ const ProductPost = () => {
   return (
     <div className='flex justify-center'>
       <div className='w-full sm:w-96 m-2'>
-        <input type="file" name="product-img" id="product-img" accept='image/*' className='mt-16 mb-4 mx-auto' />
-        <p className='mb-2 mt-4'>Product Name</p>
-        <input type="text" name="product-name" id="product-name" className='w-full box-border border border-slate-400 rounded p-1' />
-        <p className='mb-2 mt-4'>Category</p>
-        <select name="product-category" id="product-category" className='w-full box-border border border-slate-400 rounded p-1'>
-          <option value="all" selected>all</option>
-          <option value="grocery">grocery</option>
-          <option value="book">book</option>
-        </select>
-        <p className='mb-2 mt-4'>Price</p>
-        <input type="text" name="product-price" id="product-price" className='w-full box-border border border-slate-400 rounded p-1' />
-        <p className='mb-2 mt-4'>Description</p>
-        <textarea name="product-description" id="product-description" className='w-full h-32 box-border border border-slate-400 rounded p-1'></textarea>
-        <Button />
+        <form action="#" method="post">
+          <input type="file" name="product-img" id="product-img" accept='image/*' className='mt-16 mb-4 mx-auto' />
+          <p className='mb-2 mt-4'>Product Name</p>
+          <input type="text" name="product-name" id="product-name" className='w-full box-border border border-slate-400 rounded p-1' />
+          <p className='mb-2 mt-4'>Category</p>
+          <select name="product-category" id="product-category" className='w-full box-border border border-slate-400 rounded p-1'>
+            <option value="all" selected>all</option>
+            <option value="grocery">grocery</option>
+            <option value="book">book</option>
+          </select>
+          <p className='mb-2 mt-4'>Price</p>
+          <input type="text" name="product-price" id="product-price" className='w-full box-border border border-slate-400 rounded p-1' />
+          <p className='mb-2 mt-4'>Description</p>
+          <textarea name="product-description" id="product-description" className='w-full h-32 box-border border border-slate-400 rounded p-1'></textarea>
+          <Button />
+        </form>
       </div>
     </div>
   )
