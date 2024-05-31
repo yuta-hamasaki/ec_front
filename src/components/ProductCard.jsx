420f59072bdb21267e3a8598a7a0a3d4854c6749
yuta-hamasaki <yh02052002@gmail.com>
2024-05-31 12:53:23 -0700

    fixed bug

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({ productName, shopName, price }) => {
     <div className="rounded-lg w-full md:w-1/3 lg:w-1/4 p-4 m-2 shadow-xl">
       <div className="flex flex-col items-center">
         <img
-          src="https://images.unsplash.com/photo-1695048401313-d7153c37d6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://source.unsplash.com/random"
           alt="product"
           className="rounded-md w-full h-auto"
         />
