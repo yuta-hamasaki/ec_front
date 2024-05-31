6e294adf9552b01531307ebe0552a5647dbae08b
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-05-30 18:41:25 -0700

    add: import hero jsx as header component

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const Profile = () => {
+  return (
+    <div>
+      <Hero />
+      <p>Profile</p>
+    </div>
+  );
+};
+
+export default Profile;
