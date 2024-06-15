efab29817e76fbd2c72b446109338a8da0934e5a
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-06-15 16:03:00 -0700

    add: give profile path to Route link in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Top from './pages/Top';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Top from "./pages/Top";
 import Page404 from "./pages/Page404";
 import ProductPost from "./pages/ProductPost";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Top/>}/>
+        <Route path="/" element={<Top />} />
         {/*ログインページ */}
         {/*サインアップページ */}
         {/*商品詳細*/}
         {/*商品投稿 */}
-        <Route path="/productpost" element={<ProductPost/>}/>
+        <Route path="/productpost" element={<ProductPost />} />
+        <Route path="/profile" element={<Profile />} />
         {/*プロフィール*/}
         {/*アカウント設定 */}
-        <Route path="*" element={<Page404/>}/> {/*404ページ */}
+        <Route path="*" element={<Page404 />} /> {/*404ページ */}
       </Routes>
     </Router>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Hero from "../components/Hero";
 
 const Profile = () => {
   return (
     <div>
-      <Hero />
       <p>Profile</p>
     </div>
   );
